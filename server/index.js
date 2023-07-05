1dbf9ec708a922c8f55c23a9e38ca4269ca74b54
Uzair Hussnain <uzairhussnain98@gmail.com>
2023-07-05 18:02:14 +0500

    Initial commit

--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "7cde455607535f6da730": 100,
+  "1bb66262df0ce3f3cf5a": 50,
+  a9b43d78cc38f693c168: 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,7 +22,26 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+
+  const { sender, recipient, amount, signature, senderPublicKey } = req.body;
+
+  const uint8Pub = new Uint8Array(Object.keys(senderPublicKey).length);
+  Object.values(senderPublicKey).forEach((value, i) => (uint8Pub[i] = value));
+
+  const message = toHex(hashMessage(sender + "-" + amount + "-" + recipient));
+
+  let parsedSign = JSON.parse(signature);
+  parsedSign.r = BigInt(parsedSign.r);
+  parsedSign.s = BigInt(parsedSign.s);
+
+  const recovered = secp.secp256k1.verify(parsedSign, message, uint8Pub);
+
+  if (!recovered) {
+    res
+      .status(400)
+      .send({ message: "You are not the owner!" });
+    return;
+  }
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
@@ -42,3 +64,8 @@ function setInitialBalance(address) {
     balances[address] = 0;
   }
 }
+
+const hashMessage = (message) => {
+  const bytes = utf8ToBytes(message);
+  return keccak256(bytes);
+};
